68ae7b7d2349d422de709eb37176051b49eab9bd
James Wilkinson <james@jerw.ca>
2021-05-31 21:05:36 -0400

    Add Stats Page

--- src/containers/Stats.tsx
+++ src/containers/Stats.tsx
@@ -1,10 +1,27 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import SteamInstructions from "../assets/steam-instructions.jpg";
+import { Button, Form } from "react-bootstrap";
 
 export const Stats: FC = () => {
+  const initialValues = { steamId: "" };
+  const [values, setValues] = useState(initialValues);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    getStats(values.steamId);
+  };
+  const getStats = async (steamId: string) => {
+    const getReq = await fetch(
+      `http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=381210&key=3CB4E53F9792C36F39D9131612BD780F&steamid=${steamId}`
+    );
+    const dbdStats = await getReq.json();
+    console.log(dbdStats);
+  };
   return (
     <Container>
       <Row>
@@ -22,7 +39,26 @@ export const Stats: FC = () => {
           </p>
         </Col>
       </Row>
-      <Row>Form</Row>
+      <Row>
+        <Form onSubmit={onSubmit}>
+          <Form.Group>
+            <Form.Label>Steam Id</Form.Label>
+            <Form.Control
+              name="steamId"
+              value={values.steamId}
+              onChange={onChange}
+              type="number"
+              placeholder="Enter Steam ID"
+            />
+            <Form.Text className="text-muted">
+              We don't store any data
+            </Form.Text>
+          </Form.Group>
+          <Button variant="link" type="submit">
+            Get My Stats
+          </Button>
+        </Form>
+      </Row>
     </Container>
   );
 };
